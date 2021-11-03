3ddc5036e6ff16362a4a6bc5db30c0e43b70f6ba
andrsrjld <andreasallbuqori@gmail.com>
2021-11-03 12:00:13 +0700

    test 2

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          TESTING AUTOMATION
+          TESTING AUTOMATION 2
         </p>
         <a
           className="App-link"
